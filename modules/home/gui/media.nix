17ed56e3e9c06e9641b4eeec538659d1657a9e42
ryhndastra <ryhndastra@gmail.com>
2026-08-15 15:20:20 +0700

    feat(media): add OBS Studio with Wayland and PipeWire plugins

--- modules/home/gui/media.nix
+++ modules/home/gui/media.nix
@@ -15,5 +15,12 @@
     python3
     swappy
     wf-recorder
+    (pkgs.wrapOBS {
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-pipewire-audio-capture  # capture audio dari app spesifik
+        obs-gstreamer               # screen capture Wayland via PipeWire
+        wlrobs                      # Wayland screen capture (wlroots)
+      ];
+    })
   ];
 }
