0f43a166a1532faa71b4c063283e4616104f54ba
ryhndastra <ryhndastra@gmail.com>
2026-08-17 21:04:09 +0700

    fix(spicetify): use fetchgit to bypass GitHub zip rate limit 429

--- modules/home/spicetify.nix
+++ modules/home/spicetify.nix
@@ -8,11 +8,10 @@ in
     enable = true;
     theme = {
       name = "SpicetifyCat";
-      src = pkgs.fetchFromGitHub {
-        owner = "Adrien5902";
-        repo = "SpicetifyCat";
+      src = pkgs.fetchgit {
+        url = "https://github.com/Adrien5902/SpicetifyCat.git";
         rev = "c0e4c14e4ad705091cf8f45637ad94dd816fa228";
-        sha256 = "sha256-9oqR78lf6rQtzGHHuPz/3kmLv+upJUzZ6UDEEjGexvU=";
+        hash = "sha256-BRYJmeAmEwCYVJxXiclYymljhog7lW2xEtBeMTpPcOg=";
       };
     };
     colorScheme = "Purple";
