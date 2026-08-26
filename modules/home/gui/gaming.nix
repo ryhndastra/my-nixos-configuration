039dc0b05cc0131dad105679d9bcc78e3fdf926b
ryhndastra <ryhndastra@gmail.com>
2026-08-26 20:41:17 +0700

    feat(gaming): add protontricks package

--- modules/home/gui/gaming.nix
+++ modules/home/gui/gaming.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     gamescope
     mangohud
+    protontricks
     protonup-qt
     # Note: Roblox is installed via Flatpak (Sober).
     # To install: flatpak install flathub org.vinegarhq.Sober
