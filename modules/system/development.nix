aa490ece58284d7ca81cfc6e5193eb96e04bd6fb
ryhndastra <ryhndastra@gmail.com>
2026-09-03 14:05:20 +0700

    feat(dev): add development module with nix-ld, android-tools, jdk17, chrome, and flutter env vars

---
+++ modules/system/development.nix
@@ -0,0 +1,75 @@
+{ pkgs, ... }:
+
+{
+  # Nix-LD: Allows generic precompiled dynamically linked ELF binaries (like Android SDK adb, aapt2, emulator) to run on NixOS
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      zlib
+      fuse3
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      atk
+      cairo
+      cups
+      curl
+      dbus
+      expat
+      fontconfig
+      freetype
+      gdk-pixbuf
+      glib
+      gtk3
+      icu
+      libGL
+      libappindicator-gtk3
+      libdrm
+      libglvnd
+      libnotify
+      libpulseaudio
+      libunwind
+      libusb1
+      libuuid
+      libxkbcommon
+      libxml2
+      mesa
+      nspr
+      nss
+      openssl
+      pango
+      pipewire
+      systemd
+      vulkan-loader
+      xorg.libX11
+      xorg.libXScrnSaver
+      xorg.libXcomposite
+      xorg.libXcursor
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libxcb
+      xorg.libxkbfile
+      xorg.libxshmfence
+    ];
+  };
+
+  # Development tools for Flutter & Android
+  environment.systemPackages = with pkgs; [
+    android-tools
+    jdk17
+    google-chrome
+  ];
+
+  # Environment variables for Flutter / Android toolchain
+  environment.sessionVariables = {
+    JAVA_HOME = "${pkgs.jdk17}";
+    ANDROID_HOME = "/home/sho/Android/Sdk";
+    CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  };
+}
